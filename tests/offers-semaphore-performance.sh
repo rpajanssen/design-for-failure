2e8660bb821211b9b2ffde060921b282b23b785a
rpajanssen <rpa.janssen@hotmail.com>
2020-11-28 12:29:56 +0100

    revert back to older versions to prevent runtime error; update presentations; update demo readme

--- tests/offers-semaphore-performance.sh
+++ tests/offers-semaphore-performance.sh
@@ -1,1 +1,1 @@
-seq 1 100000 | xargs -n1 -P1000 bash -c 'url="http://localhost:8080/offers/semaphore"; curl -O -s $url'
+seq 1 100000 | xargs -n1 -P200 bash -c 'url="http://localhost:8080/offers/semaphore"; curl -O -s $url'
